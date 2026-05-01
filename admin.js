7e01031a53ef26eef63927c2854de2a7bf7f3a57
Nduduzo Ngubane <nduduzongubs@gmail.com>
2026-05-01 18:37:34 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -10,13 +10,24 @@ function loadAdmin() {
   }
 
   bookings.forEach((b, index) => {
+    let statusColor =
+      b.status === "Approved" ? "limegreen" :
+      b.status === "Rejected" ? "red" :
+      "orange";
+
     let li = document.createElement("li");
 
     li.innerHTML = `
       <strong>${b.name}</strong> - ${b.service}<br/>
       ${b.date} | ${b.phone}<br/>
       📍 ${b.address}<br/>
+
+      <p>Status: <span style="color:${statusColor}">${b.status}</span></p>
+
+      <button onclick="approve(${index})">Approve</button>
+      <button onclick="reject(${index})">Reject</button>
       <button onclick="deleteBooking(${index})">Delete</button>
+
       <hr/>
     `;
 
@@ -24,10 +35,26 @@ function loadAdmin() {
   });
 }
 
+function approve(index) {
+  bookings[index].status = "Approved";
+  save();
+  loadAdmin();
+}
+
+function reject(index) {
+  bookings[index].status = "Rejected";
+  save();
+  loadAdmin();
+}
+
 function deleteBooking(index) {
   bookings.splice(index, 1);
-  localStorage.setItem("cleanslate_bookings", JSON.stringify(bookings));
+  save();
   loadAdmin();
+}
+
+function save() {
+  localStorage.setItem("cleanslate_bookings", JSON.stringify(bookings));
 }
 
 window.onload = loadAdmin;
